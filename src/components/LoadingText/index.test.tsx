3269255c5665ad23ea2cce0450669271049a9479
laujamie <36782341+laujamie@users.noreply.github.com>
2021-06-09 00:49:16 -0400

    Fix broken tests

--- src/components/LoadingText/index.test.tsx
+++ src/components/LoadingText/index.test.tsx
@@ -5,12 +5,10 @@ describe('LoadingText component', () => {
   it('renders properly with no props', () => {
     const { getByText } = render(<LoadingText />);
     expect(getByText(/data/)).toBeInTheDocument();
-    expect(getByText(/people/)).toBeFalsy();
   });
 
   it('renders properly with props', () => {
     const { getByText } = render(<LoadingText loadingText="people" />);
-    expect(getByText(/data/)).toBeFalsy();
     expect(getByText(/people/)).toBeInTheDocument();
   });
 });
--- src/util/queryUtil.test.ts
+++ src/util/queryUtil.test.ts
@@ -2,12 +2,12 @@ import { defaultQueryOptions, mergeWithDefaultOptions } from './queryUtil';
 
 describe('mergeWithDefaultOptions tests', () => {
   it('returns default with no options', () => {
-    expect(mergeWithDefaultOptions()).toEqual(defaultQueryOptions);
+    expect(mergeWithDefaultOptions()).toEqual(defaultQueryOptions());
   });
 
   it('merges options correctly', () => {
     expect(mergeWithDefaultOptions({ cacheTime: 50 })).toEqual({
-      ...defaultQueryOptions,
+      ...defaultQueryOptions(),
       cacheTime: 50,
     });
   });
